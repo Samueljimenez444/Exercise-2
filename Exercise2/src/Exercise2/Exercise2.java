ceb59e6b2fabeef579f18e1bd97ed08fb709c783
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-09-20 14:00:12 +0200

    Second Exercise Java

---
+++ Exercise2/src/Exercise2/Exercise2.java
@@ -0,0 +1,12 @@
+package Exercise2;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		System.out.println("¿Hello What its your name?");
+		System.out.println("Hi my name is Samuel.");
+		System.out.println("Nice to meet you Samuel my name is Victor");
+
+	}
+
+}
